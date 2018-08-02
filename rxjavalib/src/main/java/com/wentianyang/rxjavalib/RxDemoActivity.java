3a761286a42c23c3287e12dac0e9a3143161d3f1
wentianyang <yqqq2015@gmail.com>
2018-08-02 14:39:20 +0800

    Rx简单使用....

--- rxjavalib/src/main/java/com/wentianyang/rxjavalib/RxDemoActivity.java
+++ rxjavalib/src/main/java/com/wentianyang/rxjavalib/RxDemoActivity.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import java.util.concurrent.TimeUnit;
 
 public class RxDemoActivity extends AppCompatActivity {
@@ -25,10 +31,13 @@ public class RxDemoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_demo);
 
-        Button button = findViewById(R.id.btn_rx);
+        final Button button = findViewById(R.id.btn_rx);
+        EditText account = findViewById(R.id.et_account);
+        EditText pwd = findViewById(R.id.et_pwd);
+        TextView title = findViewById(R.id.tv_title);
 
         RxView.clicks(button)
-            .throttleFirst(3,  TimeUnit.SECONDS)
+            .throttleFirst(3, TimeUnit.SECONDS)
             .subscribe(new Observer<Object>() {
                 @Override
                 public void onSubscribe(Disposable d) {
@@ -48,6 +57,39 @@ public class RxDemoActivity extends AppCompatActivity {
                 @Override
                 public void onComplete() {
                     Log.d(TAG, "onComplete: ");
+                }
+            });
+
+        /**
+         * 使用combineLatest来做表单验证
+         */
+        Observable.combineLatest(RxTextView.textChanges(account),
+            RxTextView.textChanges(pwd), new BiFunction<CharSequence, CharSequence, Boolean>() {
+                @Override
+                public Boolean apply(CharSequence account, CharSequence pwd) throws Exception {
+                    return !TextUtils.isEmpty(account) && !TextUtils.isEmpty(pwd);
+                }
+            })
+            .subscribe(new Observer<Boolean>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+
+                }
+
+                @Override
+                public void onNext(Boolean aBoolean) {
+                    Log.d(TAG, "combineLatest onNext : " + aBoolean);
+                    button.setClickable(aBoolean);
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+
+                @Override
+                public void onComplete() {
+
                 }
             });
     }
