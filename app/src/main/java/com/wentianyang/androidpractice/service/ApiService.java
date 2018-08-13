17081f423b67d12508a9af4491debffc7f39d31e
wentianyang <yqqq2015@gmail.com>
2018-08-13 17:18:28 +0800

    修改配置

---
+++ app/src/main/java/com/wentianyang/androidpractice/service/ApiService.java
@@ -0,0 +1,13 @@
+package com.wentianyang.androidpractice.service;
+
+/**
+ * @Date 创建时间:  2018/8/10
+ * @Author: YTW
+ * @Description:
+ **/
+
+public interface ApiService {
+
+//    @GET("https://www.v2ex.com/")
+//    Observable<String> getData();
+}
