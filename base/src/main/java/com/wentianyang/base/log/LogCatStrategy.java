b2d90dfb88794f1e1c69ff1e28891e3c89633b7a
wentianyang <yqqq2015@gmail.com>
2018-08-30 15:41:51 +0800

    配置 logger...

--- base/src/main/java/com/wentianyang/base/log/LogCatStrategy.java
+++ base/src/main/java/com/wentianyang/base/log/LogCatStrategy.java
@@ -15,6 +15,18 @@ public class LogCatStrategy implements LogStrategy {
 
     @Override
     public void log(int priority, @Nullable String tag, @NonNull String message) {
-        Log.println(priority, tag, message);
+        Log.println(priority, randomKey() + tag, message);
     }
+
+    private int last;
+
+    private String randomKey() {
+        int random = (int) (10 * Math.random());
+        if (random == last) {
+            random = (random + 1) % 10;
+        }
+        last = random;
+        return String.valueOf(random);
+    }
+
 }
